885abbd0d03ab6aa1d6adefd79c58d672debf48b
AlexMosby <m4443w@gmail.com>
2022-06-16 19:51:14 +0000

    adding planning

---
+++ asd-projects/pong/planning.js
@@ -0,0 +1,24 @@
+
+//Describe the gameplay: two paddles reflect a ball trying to get it past the other paddle
+
+//What are the conditions when the game begins?: paddle1, paddle2, score
+
+//Does the game have an end? If so, what are the conditions for when it ends?: when a player gets to score 11
+
+//What ifs will there be?: if the ball hits a paddle it is reflected if the ball hits the baoder it results in a point to the score
+
+//What are the visual game components? For example, in Bouncing Box, the game components were the board and the box: the backround(black), the two paddles and the ball(white)
+
+//Which will be static? (the board)
+
+//Which will be animated? (the box)
+
+//What data will you need to manage each game component? For example, in Bouncing Box, the data values were positionX, speedX, and points: postition1, position2, ballSpeed
+
+//Events / Logic handlekeyDown, handlekeyUp
+
+//What events will occur in this game? (timer events, keyboard events, clicking events?) keyboard events
+
+//How do those events affect the data of the program?: the control the two paddles
+
+//For each "event", write out the high-level logic of what will happen. It is better (and tricky) to be as specific as you can while remaining high-level: the arrow keys will control one paddle and the WASD keys will control the other
