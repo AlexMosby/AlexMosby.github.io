ac1dc6f682221b275236b38500dbb26df5ae1e22
AlexMosby <m4443w@gmail.com>
2022-06-24 20:55:07 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,7 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-  applyFilter();
+  applyFilter(reddify);
 
 
   // do not change the below line of code
@@ -32,14 +32,16 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-function applyFilter() {
+function applyFilter(filterFunction) {
   for (var r = 0; r < image.length; r++) {
-    var row = image[r];
 
-    for (var c = 0; c < og.length; c++) {
-      var value = og[c];
+    for (var c = 0; c < image[r].length; c++) {
+      var rgbString = image[r][c];
+      var rgbNumbers = rgbStringToArray(rgbString)
+      filterFunction(rgbNumbers)
+      rgbString = rgbArrayToString(rgbNumbers)
+      image[r][c] = rgbString
 
-      action[value];
     }
   }
 };
@@ -48,12 +50,26 @@ function applyFilter() {
 
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(num) {
+  if (num < 0) {
+    return 0
+  }
+  if(num > 255) {
+    return 255
+  }
+  return num
+}
 
 // TODO 3: Create reddify function
-
+function reddify(arr) {
+  arr[RED] = 200
+}
 
 // TODO 6: Create more filter functions
-
-
+function decreaseBlue(ar) {
+  ar[BLUE] = keepInBounds(ar[BLUE] - 50)
+}
+function increaseGreenByBlue(ar) {
+  ar[GREEN] =keepInBounds([BLUE] + [GREEN]);
+}
 // CHALLENGE code goes below here
